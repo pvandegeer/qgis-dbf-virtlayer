18b0f57b8a5408172d04bace9642e53b2dedbe62
pvandegeer <pvandegeer@gmail.com>
2020-07-05 12:52:14 +0200

    Update deborah_virtlayer.py

--- deborah_virtlayer.py
+++ deborah_virtlayer.py
@@ -6,17 +6,8 @@
  Maak een virtuele laag aan voor de kopgegevens van Deborah
                              -------------------
         begin                : 2017-05-16
-        copyright            : (C) 2017 by P. van de Geer (Archol)
-        email                : p.vandegeer@archol.nl
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
+        copyright            : (C) MIT 2017 by P. van de Geer
+        email                : -
  ***************************************************************************/
 """
 from PyQt4.QtGui import QFileDialog
